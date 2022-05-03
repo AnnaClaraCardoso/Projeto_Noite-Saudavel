6d0d3ab7f5fa000cdc2889de6cf6d9fb5c5fdc44
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-05-03 19:55:27 -0300

    Update index.js

--- src/screens/Statistics/index.js
+++ src/screens/Statistics/index.js
@@ -57,26 +57,6 @@ const exemChart = new Chart(exemCanvas, config);
 
 
 // Fazendo elementos que farão parte da página
-const today = new Date();
-const dd = today.getDate();
-const mm = today.getMonth() + 1; //January is 0!
-const yyyy = today.getFullYear();
-
-if (dd < 10) {
-   dd = '0' + dd;
-}
-
-if (mm < 10) {
-   mm = '0' + mm;
-} 
-    
-today = yyyy + '-' + mm + '-' + dd;
-
-const startDateInput = document.getElementById('date-start')
-const endDateInput = document.getElementById('date-end')
-
-startDateInput.setAttribute("max", today);
-endDateInput.setAttribute("max", today);
 
 function filterData(params) {
   const startValueDate = new Date(startDateInput.value);
