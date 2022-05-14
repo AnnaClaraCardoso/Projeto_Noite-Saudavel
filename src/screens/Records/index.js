4c46a9ea5dc1f4aad3efbb7dc0e5ba65a09a1853
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-05-14 00:18:13 -0300

    Update index.js

--- src/screens/Records/index.js
+++ src/screens/Records/index.js
@@ -1,4 +1,5 @@
 const modal = document.querySelector('.modal');
+let num;
 
 function openModal() {
   const newModal = modal.style.display = "block";
@@ -8,7 +9,7 @@ function closeModal() {
   const eventModalClose = modal.style.display = 'none'
 }
 
-const tagsInput = document.querySelector('#tags-input');
+const tagsInput = document.querySelector('#input-sleepFactors');
 
 // initialize Tagify on the above input node reference
 const tagify = new Tagify(tagsInput, {
@@ -22,3 +23,57 @@ const tagify = new Tagify(tagsInput, {
       highlightFirst: true
   }
 })
+
+function pickHourValues(hour) {
+  const hours = Number(hour.split(':')[0]);
+  const minutes = Number(hour.split(':')[1]);
+  return hours + (minutes / 60);
+}
+
+function appendRegister() {
+  const sleepAt = document.querySelector('#input-sleepAt').value;
+  const wakeUpAt = document.querySelector('#input-wakeUpAt').value;
+  const sleepFactors = [...tagify.value.map(factor => factor.value)];
+
+  const sleepAtHourValue = pickHourValues(sleepAt);
+  const wakeUpAtHourValue = pickHourValues(wakeUpAt);
+
+
+  if (sleepAtHourValue > wakeUpAtHourValue) {
+    sleepDuration = (wakeUpAtHourValue+12) - (sleepAtHourValue-12);
+  } else {
+    sleepDuration = wakeUpAtHourValue - sleepAtHourValue;
+  }
+
+  // console.log(sleepDuration)
+  hours = sleepDuration.toString().split('.')[0]
+  minutes = (((Number(sleepDuration.toString().split('.')[1])) * 60)/10);
+
+  console.log(`${hours}:${minutes}`)
+
+  registerHTML = `
+    <li>
+      <table class="table-ultima">
+        <div class="button-h1">
+          <h1 class="h1-d">Dia mês, ano</h1>
+        </div>
+        <tr class="cabeçalho-list">
+          <td>Hora em que dormiu</td>
+          <td>Hora que acordou</td>
+          <td>Tempo de sono</td>
+          <td>Fator influenciador</td>
+        </tr>
+        <tr class="dados">
+          <td>${sleepAt}</td>
+          <td>${wakeUpAt}</td>
+          <td>${hours}:${minutes}</td>
+          <td class="fator">Exemplo</td>
+        </tr>
+      </table>
+    </li>
+  `;	
+
+  console.log(sleepAt)
+  console.log(wakeUpAt)
+  console.log(sleepFactors)
+}
