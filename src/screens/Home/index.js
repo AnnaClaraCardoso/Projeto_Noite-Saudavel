72141f6b9a2f9b3ae8765f53311dada7434fbf6a
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-05-24 14:31:08 -0300

    Create index.js

---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+// Controle do modal de adicionar registro
+const modal = document.querySelector('.modal');
+
+function openModal() {
+  const newModal = modal.style.display = "block";
+}
+
+function closeModal() {
+  const eventModalClose = modal.style.display = 'none'
+}
