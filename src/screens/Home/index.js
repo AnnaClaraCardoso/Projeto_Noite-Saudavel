01b54a4435cabd7b1357909c07d8309e7721422f
AnnaClaraCardoso <84738118+AnnaClaraCardoso@users.noreply.github.com>
2022-05-24 22:11:57 -0300

    Update index.js

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,9 +2,25 @@
 const modal = document.querySelector('.modal');
 
 function openModal() {
-  const newModal = modal.style.display = "block";
+  const displayModal = modal.style.display = "block";
+  const bodyOverflow = document.body.style.overflow = "hidden";
+  
 }
 
 function closeModal() {
   const eventModalClose = modal.style.display = 'none'
+  const bodyOverflow = document.body.style.overflow = "auto";
 }
+
+const tagsInput = document.querySelector('#input-goals');
+const tagify = new Tagify(tagsInput, {
+  whitelist : ['Luminosidade', 'Temperatura', 'Ansiedade', 'Barulho'],
+  dropdown : {
+      classname     : "color-blue",
+      enabled       : 0,              // show the dropdown immediately on focus
+      maxItems      : 5,
+      position      : "text",         // place the dropdown near the typed text
+      closeOnSelect : false,          // keep the dropdown open after selecting a suggestion
+      highlightFirst: true
+  }
+})
